07b6f92f47ab5b0c5bd7132f1c5a717cc9e1d687
AnggaAhmad05 <anggaahmad9027@gmail.com>
2025-03-10 11:38:54 +0700

    `Added user management functionality to UserController and updated routes`

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SalesController;
+use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
+
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
@@ -15,6 +18,21 @@ Route::prefix('category')->group(function () {
     Route::get('baby-kid', [ProductController::class, 'babyKid'])->name('products.baby-kid');
 });
 
-Route::get('user/{id}/name/{name}', [UserController::class, 'show'])->name('user.profile');
-
 Route::get('sales', [SalesController::class, 'index'])->name('sales.index');
+
+Route::get('/level',[LevelController::class, 'index']);
+
+Route::get('kategori',[KategoriController::class, 'index']);
+
+Route::get('/user', [UserController::class, 'index']);
+
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+
+Route::put('/user/ubah_simpan{id}', [UserController::class, 'ubah_simpan']);
+
+Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+
